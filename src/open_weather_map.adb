1f9b0836ef91cd0b494b155e109dac02a156f9ae
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-04-04 10:44:24 +0200

    * Fixed application directory name.   Instead of '~/.openweathermap' it is '~/.config/openweathermap' to better conform to the   freedesktop specification.

--- src/open_weather_map.adb
+++ src/open_weather_map.adb
@@ -19,16 +19,19 @@ package body Open_Weather_Map is
    --  So we try HOME first, if that fails, we try USERPROFILE.
    Home_Unix    : constant String := "HOME";
    Home_Windows : constant String := "USERPROFILE";
-   App_Dir_Name : constant String := ".openweathermap";
+   App_Dir_Name : constant String := "openweathermap";
 
    Full_Application_Directory : constant String :=
      Ada.Directories.Compose
        (Containing_Directory =>
-          (if Ada.Environment_Variables.Exists (Name => Home_Unix) then
-                Ada.Environment_Variables.Value (Name => Home_Unix)
-           elsif Ada.Environment_Variables.Exists (Name => Home_Windows) then
-                Ada.Environment_Variables.Value (Name => Home_Windows)
-           else Ada.Directories.Current_Directory),
+          Ada.Directories.Compose
+            (Containing_Directory =>
+               (if Ada.Environment_Variables.Exists (Name => Home_Unix) then
+                     Ada.Environment_Variables.Value (Name => Home_Unix)
+                elsif Ada.Environment_Variables.Exists (Name => Home_Windows) then
+                     Ada.Environment_Variables.Value (Name => Home_Windows)
+                else Ada.Directories.Current_Directory),
+             Name                 => ".config"),
         Name                 => App_Dir_Name);
 
    -----------------------------------------------------------------------------
